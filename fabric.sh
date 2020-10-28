826b86d134ac2423b3bcb8f0bd263539b7d4cff4
Dawid Rusin <dawidrusin90@gmail.com>
2020-10-28 19:19:49 +0000

    No strange behaviour if symlinks already exist

--- fabric.sh
+++ fabric.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir /data/mods -p
-ln -s /data/mods mods
+ln -sfn /data/mods mods
 curl https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.6.1.51/fabric-installer-0.6.1.51.jar --output fabric-installer.jar
 java -jar fabric-installer.jar server -mcversion ${VERSION} -downloadMinecraft
 mv fabric-server-launch.jar runme.jar
--- script.sh
+++ script.sh
@@ -4,21 +4,23 @@
 ARGS=${ARGS/XMNS/$((MEMORY / 2))}
 ARGS=${ARGS/XMNX/$((MEMORY * 4 / 5))}
 
-# make sure the important dirs exist and make softlinks to the workdir to avoid copying of huge files
-mkdir /data/world -p
-ln -s /data/world world
-mkdir /data/world_nether -p
-ln -s /data/world_nether world_nether
-mkdir /data/world_the_end -p
-ln -s /data/world_the_end world_the_end
-mkdir /data/logs -p
-ln -s /data/logs logs
-
 # accept minecraft server eula
 echo "eula=true" > eula.txt
 
 # copy all flat data to workdir
 cp /data/* ./
+
+# bind large directories to avoid copying huge files
+mkdir /data/logs -p
+ln -sfn /data/logs logs
+if [ ${TYPE} != 'waterfall' ]; then
+    mkdir /data/world -p
+    ln -sfn /data/world world
+    mkdir /data/world_nether -p
+    ln -sfn /data/world_nether world_nether
+    mkdir /data/world_the_end -p
+    ln -sfn /data/world_the_end world_the_end
+fi
 
 # download server jar and do specific setup
 case ${TYPE} in
@@ -27,6 +29,9 @@ paper)
     ;;
 fabric)
     ./fabric.sh
+    ;;
+waterfall)
+    ./waterfall.sh
     ;;
 esac
 
