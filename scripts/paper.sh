da70b00397203ec02c60024e21f0d3c85c568efc
Dawid Rusin <dawidrusin90@gmail.com>
2021-06-14 21:55:40 +0200

    Migrated paper and waterfall to new dl API

--- scripts/paper.sh
+++ scripts/paper.sh
@@ -3,5 +3,6 @@
 export PLUGINS_FOLDER_NAME="plugins"
 
 if [ ! -f $JAR_NAME ]; then
-    wget https://papermc.io/api/v1/paper/$MC_VERSION/latest/download -O $JAR_NAME
+    build=$(curl https://papermc.io/api/v2/projects/paper/versions/$MC_VERSION | grep -oP '\"builds\"\:\[.*?\]' | grep -oP '\d*?\]' | grep -oP '\d*')
+    wget https://papermc.io/api/v2/projects/paper/versions/$MC_VERSION/builds/$build/downloads/paper-$MC_VERSION-$build.jar -O $JAR_NAME
 fi
--- scripts/waterfall.sh
+++ scripts/waterfall.sh
@@ -6,7 +6,9 @@ export WORLDS=""
 mkdir /data/modules -p
 ln -sfn /data/modules modules
 
-versionArr=(${VERSION//./ })
 if [ ! -f $JAR_NAME ]; then
-    wget https://papermc.io/api/v1/waterfall/${versionArr[0]}.${versionArr[1]}/latest/download -O $JAR_NAME
+    versionArr=(${MC_VERSION//./ })
+    waterfallVersion=(${versionArr[0]}.${versionArr[1]})
+    build=$(curl https://papermc.io/api/v2/projects/waterfall/versions/$waterfallVersion | grep -oP '\"builds\"\:\[.*?\]' | grep -oP '\d*?\]' | grep -oP '\d*')
+    wget https://papermc.io/api/v2/projects/waterfall/versions/$waterfallVersion/builds/$build/downloads/waterfall-$waterfallVersion-$build.jar -O $JAR_NAME
 fi
