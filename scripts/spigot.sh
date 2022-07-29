07e154204874724e534d87a103697fa04cf51f3e
Dawid Rusin <dawidrusin90@gmail.com>
2022-07-29 09:17:50 +0200

    make spigot work in docker

--- scripts/spigot.sh
+++ scripts/spigot.sh
@@ -9,7 +9,7 @@ if [ ! -f $JAR_NAME ]; then
     cd buildTools
     wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools.jar
     echo "########### Starting Spigot build tools, this might take a while! #################"
-    java -jar BuildTools.jar --rev $MC_VERSION >/dev/null
+    $JAVA_PATH -Xms${MEMORY}M -Xmx${MEMORY}M -jar BuildTools.jar --rev $MC_VERSION >/dev/null
     echo "########### Spigot build done #################"
     mv spigot-$MC_VERSION.jar ../$JAR_NAME
     cd ..
