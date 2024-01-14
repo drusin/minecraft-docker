c79599e5acb9ec725baeb032b177194471aa795d
Dawid Rusin <dawidrusin90@gmail.com>
2024-01-14 16:19:19 +0100

    remove fabric libraries folder when downloading a new version

--- scripts/fabric.js
+++ scripts/fabric.js
@@ -3,16 +3,21 @@
 import { $, fetch } from 'zx';
 
 export default async function(E = process.env) {
+    //delete old lib folder
+    await $`rm -rfd $DATA_DIR/libraries`;
+
     // Find the right fabric installer for the requested Minecraft version
     const response = await fetch('https://maven.fabricmc.net/net/fabricmc/fabric-installer/maven-metadata.xml');
     const versionXML = await response.text();
     const versions = versionXML.split('\n').filter(str => str.match(/<version>.*?<\/version>/));
     const latestVersion = versions[versions.length - 1].replace(/<\/?version>/g, '').trim();
+
     // Download the installer and start the installation
     await $`wget https://maven.fabricmc.net/net/fabricmc/fabric-installer/${latestVersion}/fabric-installer-${latestVersion}.jar -O fabric-installer.jar`;
-
     const args = `-jar fabric-installer.jar server -mcversion ${E.MC_VERSION} -downloadMinecraft`;
     await $`./run-java.sh ${args}`
+
+    //copy files and folders
     await $`mv fabric-server-launch.jar $JAR_NAME`;
     await $`cp -r libraries $DATA_DIR/libraries`;
     await $`cp server.jar $DATA_DIR/`;
